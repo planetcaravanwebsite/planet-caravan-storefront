35506db004edb1c10e015cff6760ada7500a6c94
joshrps <josh@rndpxl.com>
2021-03-14 15:41:06 -0400

    Lint fixes

--- src/views/Search/View.tsx
+++ src/views/Search/View.tsx
@@ -1,28 +1,15 @@
 import * as React from "react";
-import { useIntl } from "react-intl";
 import { RouteComponentProps } from "react-router";
 
-import { prodListHeaderCommonMsg } from "@temp/intl";
-import { IFilters } from "@types";
 import { StringParam, useQueryParam } from "use-query-params";
-import { NotFound, OfflinePlaceholder } from "../../components";
-import NetworkStatus from "../../components/NetworkStatus";
-import { PRODUCTS_PER_PAGE } from "../../core/config";
-import {
-  convertSortByFromString,
-  convertToAttributeScalar,
-  getGraphqlIdFromDBId,
-  maybe,
-} from "../../core/utils";
 import Page from "./Page";
-import { TypedSearchProductsQuery } from "./queries";
 
 type ViewProps = RouteComponentProps<{
   id: string;
 }>;
 
 export const View: React.FC<ViewProps> = ({ match }) => {
-  const [search, setSearch] = useQueryParam("q", StringParam);
+  const [search] = useQueryParam("q", StringParam);
 
   return <Page search={search} />;
 };
