66d42f360e8682293e483d9af726dce1e3e1ac37
Andy <andy@Andys-MacBook-Pro.local>
2021-04-02 13:51:50 -0400

    build fix

--- src/views/Home/View.tsx
+++ src/views/Home/View.tsx
@@ -14,8 +14,7 @@ const View: React.FC = () => (
           <MetaWrapper
             meta={{
               description: data.shop ? data.shop.description : "",
-              title:
-                "Cincinnati Smoke Shop | Planet Caravan.",
+              title: "Cincinnati Smoke Shop | Planet Caravan.",
             }}
           >
             <Page
