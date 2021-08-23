103f857c6169c66b1cace50106e6b6c0fed5f181
Andy <andy@Andys-MacBook-Pro.local>
2021-08-23 19:24:52 -0400

    switching banner for both home and sub pages

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -306,11 +306,7 @@ const MainMenu: React.FC<MainMenuProps> = ({ demoMode, whichMenu }) => {
       </nav>
       {whichMenu !== "homePage" && (
         <>
-          <div className="coupon-bar">
-            <b>Back to School Sale:</b> 15% off entire order!! Use Code:{" "}
-            <b>BACK2SCHOOL</b>
-            <div className="exclusions">*Some exclusions apply</div>
-          </div>
+          <TakeShape position="topBanner" />
         </>
       )}
       {whichMenu !== "homePage" && <div className="bottom-bar" />}
